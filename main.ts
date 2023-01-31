fe87b360841fb5baea8dd49f96fd14cc2110b70c
martinaaloopez <122534401+martinaaloopez@users.noreply.github.com>
2023-01-31 13:47:43 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,74 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    while (enviado == 0) {
+        basic.pause(100)
+    }
+    if (receivedNumber == 3) {
+        basic.clearScreen()
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+    if (receivedNumber == 1) {
+        basic.clearScreen()
+        basic.showLeds(`
+            . # # # .
+            # # # # #
+            # # # # #
+            # # # # #
+            . # # # .
+            `)
+    }
+    if (receivedNumber == 2) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.clearScreen()
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # # # # #
+        # # # # #
+        . # # # .
+        `)
+    radio.sendNumber(1)
+    enviado = 1
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    radio.sendNumber(2)
+    enviado = 1
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # # . . #
+        # # . # .
+        . . # . .
+        # # . # .
+        # # . . #
+        `)
+    radio.sendNumber(3)
+    enviado = 1
+})
+let enviado = 0
+radio.setGroup(79)
+enviado = 0
 basic.forever(function () {
 	
 })
